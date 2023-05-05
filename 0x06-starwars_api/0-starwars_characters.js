e7b7e8f703785858b259f4a569ced7faba968f44
favvie <favourabangwu@gmail.com>
2023-05-05 04:18:05 +0100

    refactor code  + semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,23 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
 
-function Characters(movieID) {
-    movieID = process.argv.slice(2)[0]
-    let url = `https://swapi-api.alx-tools.com/api/films/${movieID}`
-    request(url, (error, response, body) => {
-        if (error) console.error(error)
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv.slice(2)[0]}`;
 
-        const characters = JSON.parse(body).characters
-        for (let character of characters) {
-            request(character, (error, response, body) => {
-                let name = JSON.parse(body).name
-                console.log(name)
-            })
-        }
+request(url, (error, response, body) => {
+  if (error) console.error(error);
 
+  const characters = JSON.parse(body).characters;
+
+  characters.forEach(characters => {
+    request(characters, (error, resp, body) => {
+      if (error) console.error(error);
+      console.log(JSON.parse(body).name);
     });
-
-
-}
-
-Characters()
+  });
+});
